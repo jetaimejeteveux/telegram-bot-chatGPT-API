4cefcc595b948848c58dd7d2bfc4d2e2fe44bfc4
Firman Aulia <akufirman22@gmail.com>
2023-01-23 09:37:49 +0700

    add : request model and response model

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	// "context"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	// "github.com/PullRequestInc/go-gpt3"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jetaimejeteveux/tele-bot-spreadsheetAPI/models"
 	"github.com/joho/godotenv"
 )
 
@@ -37,9 +38,6 @@ func main() {
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	//picking model
-	model := "text-davinci-003"
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
@@ -47,14 +45,15 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil { // If we got a message
-			prompt := update.Message.Text
 			//create request body
-			body := strings.NewReader(fmt.Sprintf(`{
-				"model": "%s",
-				"prompt": "%s"
-			}`, model, prompt))
-
-			req, err := http.NewRequest("POST", "https://api.openai.com/v1/completions", body)
+			reqBody := models.Request{
+				ModelRequest: "text-davinci-003",
+				Prompt:       update.Message.Text,
+				Temperature:  1,
+				MaxTokens:    100,
+			}
+			jsonBody, _ := json.Marshal(reqBody)
+			req, err := http.NewRequest("POST", "https://api.openai.com/v1/completions", bytes.NewBuffer(jsonBody))
 			req.Header.Add("Content-Type", "application/json")
 			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", getAPIkey("API_KEY")))
 			if err != nil {
@@ -70,20 +69,16 @@ func main() {
 			defer resp.Body.Close()
 
 			// Read the response
-			data, err := ioutil.ReadAll(resp.Body)
+			jsonData, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
 				panic(err)
 			}
-			log.Printf(" response dari API = %v", data)
-			// Print the response
-			log.Printf(" response dari API bentuk string = %s", string(data))
 
-			log.Printf("\n ini printf 1 [%s] %s", update.Message.From.UserName, update.Message.Text)
+			var data models.TextCompletionResponse
+			json.Unmarshal([]byte(jsonData), &data)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, string(data))
-			log.Printf("\n ini printf 2[%v] %v", update.Message.Chat.ID, update.Message.Text)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, data.Choices[0].Text)
 			msg.ReplyToMessageID = update.Message.MessageID
-			log.Printf("ini printf 3 %v %v", msg.ReplyToMessageID, update.Message.MessageID)
 
 			bot.Send(msg)
 		}
